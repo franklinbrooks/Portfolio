b996374b925859d9f914008271dc08920e838a1d
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-01-31 02:46:13 -0500

    routes added

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,37 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Express',
-    env: JSON.stringify(process.env.NODE_ENV)
+    title: 'Franklin Brooks'
+  });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+router.get('/education', function(req, res, next) {
+  res.render('education', {
+    title: 'Education'
+  });
+});
+
+router.get('/employment', function(req, res, next) {
+  res.render('employment', {
+    title: 'Employment'
+  });
+});
+
+router.get('/skills', function(req, res, next) {
+  res.render('skills', {
+    title: 'Skills'
+  });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {
+    title: 'Contact'
   });
 });
 
